c5fc80e32179c77f8276837f2cf91b88709684b6
Alfred Meng <alfredfmeng@gmail.com>
2021-12-25 20:13:58 -0600

    Began adding button click functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
-console.log("the script is running!");
+const button = document.getElementById("click-me");
+
+let completed = false;
+
+button.onclick = () => {
+  if (completed) {
+    button.classList.remove("fas", "fa-check-circle");
+    button.classList.add("far", "fa-circle");
+  } else {
+    button.classList.remove("far", "fa-circle");
+    button.classList.add("fas", "fa-check-circle");
+  }
+  completed = !completed;
+};
